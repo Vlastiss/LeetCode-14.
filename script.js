4e81bba8f22ade4a25803f39520171740a0711ac
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-25 20:19:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,24 @@
  */
 
 let prefix = '';
-var longestCommonPrefix = function(strs) {
-    
-    console.log(strs[0][0])
-    
+var longestCommonPrefix = function (strs) {
+
+    if (strs.length === ''){
+        return '';
+    }
+    for (let i = 0; i < strs.length; i++) {
+        { let j = 0; j < strs[i].length; j++ } {
+            if (strs[i][j] === strs[i][j] === strs[i][j]) {
+                prefix += strs[i][j];
+            }
+            return prefix
+        }
+        {
+        }
+        console.log(strs[i][0])
+        return prefix;
+    }
+
 };
 
-longestCommonPrefix(["flower","flow","flight"]);
+longestCommonPrefix(["flower", "flow", "flight"]);
