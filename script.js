f76cb629d025d193bd5a9495f15b99ec8a009887
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-25 20:07:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,23 @@
 // Input: strs = ["flower","flow","flight"]
 // Output: "fl"
 
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
 
 /**
  * @param {string[]} strs
  * @return {string}
  */
 
+let prefix = '';
 var longestCommonPrefix = function(strs) {
     
+    console.log(strs[0][0])
+    
 };
+
+longestCommonPrefix(["flower","flow","flight"]);
