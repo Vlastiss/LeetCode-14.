8a55f57c269481d89bdf68ed14a06e2142fb9d08
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-25 20:01:24 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+var longestCommonPrefix = function(strs) {
+    
+};
